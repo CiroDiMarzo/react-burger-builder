ab47afe61f18a7913e5f69de346eff9aae29240f
Ciro di Marzo <ciro@Air-di-Ciro.lan>
2019-03-05 21:12:50 +0100

    Toolbar and logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Aux from "../../hoc/Auxiliary";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 import classes from './Layout.module.css';
 
 const layout = (props) => {
     return (
         <Aux>
-            {/* <div>Toolbar, SideDrawer</div> */}
+            <Toolbar></Toolbar>
             <main className={classes.Content}>
                 {props.children}
             </main>
